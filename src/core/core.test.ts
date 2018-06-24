8ac9c676f5c9536cd56339176b6dbacc314bfe15
Heather Park <hthr.prk@gmail.com>
2018-06-23 18:37:57 -0700

    Write tests for and implement vote function

--- src/core/core.test.ts
+++ src/core/core.test.ts
@@ -1,17 +1,27 @@
 import 'mocha';
 import { expect } from 'chai';
 
-import { next, setEntries } from './core';
+import { next, setEntries, vote } from './core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
     it('adds the entries to the state', () => {
-      const state = {};
+      const state = {
+        entries: [],
+        vote: {
+          pair: [],
+          tally: {}
+        }
+      };
       const entries = ['Germany', 'Spain'];
       const nextState = setEntries(state, entries);
 
       expect(nextState).to.deep.equal({
-        entries: ['Germany', 'Spain']
+        entries: ['Germany', 'Spain'],
+        vote: {
+          pair: [],
+          tally: {}
+        }
       });
     });
   });
@@ -19,7 +29,11 @@ describe('application logic', () => {
   describe('next', () => {
     it('takes the next two entries under vote', () => {
       const state = {
-        entries: ['Germany', 'Spain', 'England']
+        entries: ['Germany', 'Spain', 'England'],
+        vote: {
+          pair: [],
+          tally: {}
+        }
       };
       const nextState = next(state);
 
@@ -28,6 +42,54 @@ describe('application logic', () => {
           pair: ['Germany', 'Spain']
         },
         entries: ['England']
+      });
+    });
+  });
+
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = {
+        vote: {
+          pair: ['Germany', 'Spain'],
+          tally: {}
+        },
+        entries: []
+      };
+      const nextState = vote(state, 'Germany');
+
+      expect(nextState).to.deep.equal({
+        vote: {
+          pair: ['Germany', 'Spain'],
+          tally: {
+            'Germany': 1
+          }
+        },
+        entries: []
+      });
+    });
+
+    it('adds to exsting tally for the voted entry', () => {
+      const state = {
+        vote: {
+          pair: ['Germany', 'Spain'],
+          tally: {
+            'Germany': 3,
+            'Spain': 2
+          }
+        },
+        entries: []
+      };
+      const nextState = vote(state, 'Germany');
+
+      expect(nextState).to.deep.equal({
+        vote: {
+          pair: ['Germany', 'Spain'],
+          tally: {
+            'Germany': 4,
+            'Spain': 2
+          }
+        },
+        entries: []
       });
     });
   });
--- src/core/core.ts
+++ src/core/core.ts
@@ -1,11 +1,21 @@
-export function setEntries(state: {}, entries: string[]): {} {
+interface EntriesState {
+  entries: string[];
+  vote: {
+    pair: string[];
+    tally: {
+      [key: string]: number;
+    }
+  }
+}
+
+export function setEntries(state: EntriesState, entries: string[]): {} {
   return {
     ...state,
     entries
   }
 }
 
-export function next(state: { entries: string[] }): {} {
+export function next(state: EntriesState): {} {
   const entries: string[] = state.entries;
 
   return {
@@ -14,5 +24,22 @@ export function next(state: { entries: string[] }): {} {
       pair: [entries[0], entries[1]]
     },
     entries: [...entries.slice(2)]
+  };
+}
+
+export function vote(state: EntriesState, entry: string): {} {
+  const entryTally = state.vote.tally[entry] 
+    ? state.vote.tally[entry] + 1 
+    : 1;
+
+  return {
+    ...state,
+    vote: {
+      ...state.vote,
+      tally: {
+        ...state.vote.tally,
+        [entry]: entryTally
+      }
+    }
   };
 }
