d11f2ced1a1a541c1a3c5799fe12b67bcde96aff
Heather Park <hthr.prk@gmail.com>
2018-06-23 17:41:33 -0700

    Write test for and implement next function

--- src/core/core.test.ts
+++ src/core/core.test.ts
@@ -1,6 +1,7 @@
-import { setEntries } from './core';
 import 'mocha';
 import { expect } from 'chai';
+
+import { next, setEntries } from './core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -11,6 +12,22 @@ describe('application logic', () => {
 
       expect(nextState).to.deep.equal({
         entries: ['Germany', 'Spain']
+      });
+    });
+  });
+
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = {
+        entries: ['Germany', 'Spain', 'England']
+      };
+      const nextState = next(state);
+
+      expect(nextState).to.deep.equal({
+        vote: {
+          pair: ['Germany', 'Spain']
+        },
+        entries: ['England']
       });
     });
   });
--- src/core/core.ts
+++ src/core/core.ts
@@ -4,3 +4,15 @@ export function setEntries(state: {}, entries: string[]): {} {
     entries
   }
 }
+
+export function next(state: { entries: string[] }): {} {
+  const entries: string[] = state.entries;
+
+  return {
+    ...state,
+    vote: {
+      pair: [entries[0], entries[1]]
+    },
+    entries: [...entries.slice(2)]
+  };
+}
