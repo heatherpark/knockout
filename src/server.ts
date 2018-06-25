0f69e28e460639f3c6131deb1df1be091a5a95d4
Heather Park <hthr.prk@gmail.com>
2018-06-24 17:41:30 -0700

    Have server listen for and emit various events

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
-export default function startServer() {
+export default function startServer(store: any) {
   const io = require('socket.io')(8090);
+
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket: any) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
 }
