833946b5253aff166a1c9752bbb95a6d95ddab28
Heather Park <hthr.prk@gmail.com>
2018-06-24 18:35:57 -0700

    Dispatch actions upon starting server

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import makeStore from './src/store';
-import startServer from './src/server'
+import startServer from './src/server';
+
+import entries from './entries';
 
 export const store = makeStore();
 startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries
+});
+
+store.dispatch({ type: 'NEXT' });
