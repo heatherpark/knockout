85a152f7e0812cf1b48ab3a871a798524af74055
Heather Park <hthr.prk@gmail.com>
2018-06-25 12:19:36 -0700

    Write vote callback test

--- src/components/Voting.test.tsx
+++ src/components/Voting.test.tsx
@@ -18,11 +18,27 @@ describe('Voting', () => {
   });
 
   it('renders a pair of buttons', () => {
-    props = { pair: ['Germany', 'Spain'] };
+    props = { 
+      pair: ['Germany', 'Spain'],
+      vote: () => {} 
+    };
     const buttons = shallowRender(props).find('button');
 
     expect(buttons.length).toBe(2);
     expect(buttons.at(0).render().text()).toBe(props.pair[0]);
     expect(buttons.at(1).render().text()).toBe(props.pair[1]);
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    const mockCallback = jest.fn();
+    props = { 
+      pair: ['Germany', 'Spain'],
+      vote: mockCallback 
+    };
+    
+    const button = shallowRender(props).find('button').first();
+    button.simulate('click');
+    
+    expect(mockCallback.mock.calls.length).toBe(1);
   });
 });
--- src/components/Voting.tsx
+++ src/components/Voting.tsx
@@ -2,13 +2,17 @@ import * as React from 'react';
 
 export interface IProps {
   pair: string[];
+  vote: (entry: string) => void;
 }
 
 const Voting: React.SFC<IProps> = (props: IProps) => {
+
   return (
     <div className="voting">
       {props.pair.map(entry =>
-        <button key={entry}>
+        <button 
+          key={entry}
+          onClick={() => props.vote(entry)}>
           <h1>{entry}</h1>
         </button>
       )}
