ec3c580f2582a94d700f7c814c66dd5f39ca651d
Heather Park <hthr.prk@gmail.com>
2018-06-26 13:25:50 -0700

    Create store with middleware

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import * as io from 'socket.io-client';
 import registerServiceWorker from './registerServiceWorker';
 
+import remoteActionMiddleware from './store/remote-action-middleware';
 import { setState } from './store/actions/actions';
 import reducer from './store/reducer';
 
 import App from './App';
 
-const store = createStore(
-  reducer,
-  devToolsEnhancer({})
-);
+const store = createStore(reducer, composeWithDevTools(
+  applyMiddleware(remoteActionMiddleware)
+));
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state => {
