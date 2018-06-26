fad6967473208bdaa2f77aeeff81305740381234
Heather Park <hthr.prk@gmail.com>
2018-06-26 07:59:34 -0700

    Create Redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
+import reducer from './store/reducer';
+
 import App from './App';
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Germany', 'Spain'],
+      tally: { 'Germany': 2 }
+    }
+  }
+});
 
 ReactDOM.render(
   <BrowserRouter>
