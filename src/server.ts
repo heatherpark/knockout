ed67a32cd5b650c6968e1ff0f533578dc8e7be96
Heather Park <hthr.prk@gmail.com>
2018-06-26 08:32:39 -0700

    Remove method call

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ export default function startServer(store: any) {
   const io = require('socket.io')(8090);
 
   store.subscribe(
-    () => io.emit('state', store.getState().toJS())
+    () => io.emit('state', store.getState())
   );
 
   io.on('connection', (socket: any) => {
-    socket.emit('state', store.getState().toJS());
+    socket.emit('state', store.getState());
     socket.on('action', store.dispatch.bind(store));
   });
 }
