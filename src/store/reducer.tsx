705e1cf269fb90a78515988dc1841b479ca96b67
Heather Park <hthr.prk@gmail.com>
2018-06-26 11:28:27 -0700

    Write function for resetting vote state

--- src/store/reducer.tsx
+++ src/store/reducer.tsx
@@ -1,6 +1,6 @@
 function vote(state, entry) {
   const currentPair = state.vote.pair;
-  
+
   if (currentPair && currentPair.includes(entry)) {
     return {
       ...state,
@@ -11,13 +11,23 @@ function vote(state, entry) {
   }
 }
 
+function resetVote(currentState, nextState) {
+  const hasVoted = currentState.hasVoted;
+  const nextPair = nextState.vote && nextState.vote.pair;
+  
+  if (hasVoted && !nextPair.includes(hasVoted)) {
+    return {
+      vote: nextState.vote
+    }
+  } else {
+    return nextState;
+  }
+}
+
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_STATE':
-      return {
-        ...state,
-        ...action.state
-      };
+      return resetVote(state, action.state);
     case 'VOTE':
       return vote(state, action.entry);
     default:
