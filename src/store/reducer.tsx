8cf296c7de4550bb2ca77f9bf431cf6e3b7e41d4
Heather Park <hthr.prk@gmail.com>
2018-06-26 07:55:01 -0700

    Write reducer case for SET_STATE action

---
+++ src/store/reducer.tsx
@@ -0,0 +1,11 @@
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_STATE':
+      return {
+        ...state,
+        ...action.state
+      };
+    default:
+      return state;
+  }
+};
