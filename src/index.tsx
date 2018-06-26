0d46076d77aec3d7fdad0f5e3c55ecaaa4ac0f33
Heather Park <hthr.prk@gmail.com>
2018-06-26 09:06:46 -0700

    Add redux dev tools extension

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,32 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import * as io from 'socket.io-client';
 import registerServiceWorker from './registerServiceWorker';
 
 import reducer from './store/reducer';
 
 import App from './App';
 
-const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Germany', 'Spain'],
-      tally: { 'Germany': 2 }
-    }
-  }
+const store = createStore(
+  reducer,
+  devToolsEnhancer({})
+);
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('connect', () => {
+  // tslint:disable-next-line:no-console
+  console.log('connected');
+  store.dispatch({
+    state: {
+      vote: {
+        pair: ['Germany', 'Spain'],
+        tally: { 'Germany': 2 }
+      }
+    },
+    type: 'SET_STATE'
+  });
 });
 
 ReactDOM.render(
