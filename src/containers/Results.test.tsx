39b36ca99846b3b07a7194017096caee9d04b7ca
Heather Park <hthr.prk@gmail.com>
2018-06-27 17:13:50 -0700

    Update for linting

--- src/containers/Results.test.tsx
+++ src/containers/Results.test.tsx
@@ -4,8 +4,8 @@ import * as Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 
 import Winner from '../components/Winner';
-import { Results } from './Results';
 import { renderedText } from '../utility/testing';
+import { Results } from './Results';
 
 Enzyme.configure({ adapter: new Adapter() });
   
