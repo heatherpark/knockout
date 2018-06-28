11c48755c3ddcc2e6579219ebfde6f10e82f323e
Heather Park <hthr.prk@gmail.com>
2018-06-27 17:39:31 -0700

    Write action constants

---
+++ src/actionTypes.ts
@@ -0,0 +1,3 @@
+export const NEXT = 'NEXT';
+export const SET_ENTRIES = 'SET_ENTRIES';
+export const VOTE = 'VOTE';
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,21 +1,17 @@
-import { 
-  EntriesState, 
-  INITIAL_STATE, 
-  setEntries, 
-  next, 
-  vote 
-} from './core';
+import * as actionTypes from './actionTypes';
+import { EntriesState, INITIAL_STATE, 
+  setEntries, next, vote } from './core';
 
 export default function reducer(
   state: EntriesState = INITIAL_STATE, 
   action
 ): {} {
   switch (action.type) {
-    case 'SET_ENTRIES':
+    case actionTypes.SET_ENTRIES:
       return setEntries(state, action.entries);
-    case 'NEXT':
+    case actionTypes.NEXT:
       return next(state);
-    case 'VOTE':
+    case actionTypes.VOTE:
       return {
         ...state,
         vote: vote(state.vote, action.entry)
