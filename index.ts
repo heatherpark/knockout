2986aa84796906833a2b4e2661d1f6ecb31b22a5
Heather Park <hthr.prk@gmail.com>
2018-06-27 17:50:40 -0700

    Use action constant

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import makeStore from './src/store';
 import startServer from './src/server';
 
+import * as actionTypes from './src/actionTypes';
 import entries from './entries';
 
 export const store = makeStore();
 startServer(store);
 
 store.dispatch({
-  type: 'SET_ENTRIES',
+  type: actionTypes.SET_ENTRIES,
   entries
 });
 
-store.dispatch({ type: 'NEXT' });
+store.dispatch({ type: actionTypes.NEXT });
--- src/store.spec.ts
+++ src/store.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 
+import * as actionTypes from './actionTypes';
 import { INITIAL_STATE } from './core';
 import makeStore from './store';
 
@@ -10,7 +11,7 @@ describe('store', () => {
     expect(store.getState()).to.deep.equal(INITIAL_STATE);
 
     store.dispatch({
-      type: 'SET_ENTRIES',
+      type: actionTypes.SET_ENTRIES,
       entries: ['Germany', 'Spain']
     });
 
